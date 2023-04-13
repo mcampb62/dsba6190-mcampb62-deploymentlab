badc61c122516061092cb58c791c31fe21a6902c
mcampb62 <31451855+mcampb62@users.noreply.github.com>
2023-04-12 20:52:15 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,148 @@
+// Tags
+locals {
+  tags = {
+    owner       = var.tag_department
+    region      = var.tag_region
+    environment = var.environment
+  }
+}
+
+// Existing Resources
+
+/// Subscription ID
+
+data "azurerm_subscription" "current" {
+}
+
+// Random Suffix Generator
+
+resource "random_integer" "deployment_id_suffix" {
+  min = 100
+  max = 999
+}
+
+// Resource Group
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.class_name}-${var.student_name}-${var.environment}-${random_integer.deployment_id_suffix.result}-rg"
+  location = var.location
+
+  tags = local.tags
+}
+
+
+// Storage Account
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}st"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  is_hns_enabled           = true
+
+  tags = local.tags
+}
+
+// CosmosDB
+
+resource "azurerm_cosmosdb_account" "db" {
+  name                = "${var.class_name}-${var.student_name}-${var.environment}-${random_integer.deployment_id_suffix.result}-db"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+
+  enable_automatic_failover = true
+
+  capabilities {
+    name = "EnableAggregationPipeline"
+  }
+
+  capabilities {
+    name = "mongoEnableDocLevelTTL"
+  }
+
+  capabilities {
+    name = "MongoDBv3.4"
+  }
+
+  capabilities {
+    name = "EnableMongo"
+  }
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+
+  geo_location {
+    location          = "eastus"
+    failover_priority = 1
+  }
+
+  geo_location {
+    location          = "westus"
+    failover_priority = 0
+  }
+}
+
+// TODO: Machine Learning Workspace
+
+resource "azurerm_application_insights" "insights" {
+  name                = "${var.class_name}-${var.student_name}-${var.environment}-${random_integer.deployment_id_suffix.result}-ins"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                = "${var.class_name}-${var.student_name}-${var.environment}-${random_integer.deployment_id_suffix.result}-kv"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tenant_id           = data.azurerm_subscription.current.tenant_id
+  sku_name            = "premium"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "workspacestorageaccount"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+/* resource "azurerm_machine_learning_workspace" "example" {
+  name                    = "example-workspace"
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  application_insights_id = azurerm_application_insights.insights.id
+  key_vault_id            = azurerm_key_vault.kv.id
+  storage_account_id      = azurerm_storage_account.example.id
+
+  identity {
+    type = "SystemAssigned"
+  }
+} */
+
+// Maps Account
+
+resource "azurerm_maps_account" "map" {
+  name                = "${var.class_name}-${var.student_name}-${var.environment}-${random_integer.deployment_id_suffix.result}-mp"
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name            = "S1"
+
+  tags = local.tags
+}
+
+// Public IP Address
+
+resource "azurerm_public_ip" "pip" {
+  name                = "${var.class_name}-${var.student_name}-${var.environment}-${random_integer.deployment_id_suffix.result}-ip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+
+  tags = local.tags
+}
